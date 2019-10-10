f38d3907091a9214fdc057cb2bf2becd4319c30c
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-10-10 11:49:18 -0400

    cors issues today again

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server.use((req , res , next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
 next();
 });
 const cors = require('cors');
