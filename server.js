cd1b33787667abce1b61f871737ba3281c7ad298
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-10-10 18:00:32 -0400

    tested on postman backend complete cors headers added

--- server.js
+++ server.js
@@ -63,13 +63,13 @@ server.get('/api/posts/', function (req, res, next) {
 })
 
 
-// server.use((req , res , next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Credentials", true);
-// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-// res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-// next();
-// });
+server.use((req , res , next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+next();
+});
 
  
 
